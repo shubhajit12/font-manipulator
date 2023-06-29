b890fb37ed0ed3b99acf7614a942b3ed116ce372
shubhajit12 <118266831+shubhajit12@users.noreply.github.com>
2023-06-29 20:36:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas=createCanvas(550,500);
+    canvas.position(850,150);
+    video=createCapture(VIDEO);
+    video.position(150,150);
+    video.size(550,500);
+
+}
+function draw(){
+    background("lightyellow");
+}
